b1d66159b58ea0d664625b6b4967d4e49b6ef6fd
Jeff Gregory <jeffrey.s.gregory@me.com>
2020-10-20 12:59:59 -0500

    Watershed script updates
    Added functionality to use the watershed's name as the dss filename.
    Also, added script name argument and try..except for importing
    `rtsutils`.

--- watershed_scripts/cwms_radar.py
+++ watershed_scripts/cwms_radar.py
@@ -2,20 +2,34 @@ import os
 import sys
 from hec.heclib.util import HecTime
 from hec.script import MessageBox
+script_name = arg2
 # Add rtsutils package to sys.path before importing
-sys.path.append(os.path.join(os.environ['APPDATA'], "rsgis"))
-from rtsutils import cavistatus, cwmsradar
+try:
+    sys.path.append(os.path.join(os.environ['APPDATA'], "rsgis"))
+    from rtsutils import cavistatus, cwmsradar
+except ImportError, ex:
+    raise
 #
 tw = cavistatus.get_timewindow()
 if tw != None:
     st, et = tw
+    print("Time window: {}".format(tw))
 else:
-    MessageBox.showError('No Forecast open to get a timewindow', 'Error')
-    raise Exception('No Forecast open to get a timewindow')
+    raise Exception('No Forecast open or in "Setup Tab"')
 cwmsdat = cwmsradar.CwmsRADAR()
 cwmsdat.begintime = cwmsdat.format_datetime(HecTime(st))
 cwmsdat.endtime = cwmsdat.format_datetime(HecTime(et))
-cwmsdat.dssfile = os.path.join(cavistatus.get_database_directory(), 'test-data.dss')
-cwmsdat.read_config(os.path.join(cavistatus.get_database_directory(), 'cwms_radar.config'))
+cwmsdat.dssfile = os.path.join(
+    cavistatus.get_database_directory(),
+    '{}-grids.dss'.format(cavistatus.get_watershed().getName())
+    )
+cwmsdat.read_config(os.path.join(
+        cavistatus.get_shared_directory(),
+        'cwms_radar.config'
+        )
+    )
 cwmsdat.set_tsids() # Reading the configutation file defines the lists but they still need to be set
 cwmsdat.run()
+MessageBox.showInformation(
+    "Script '{}' done!".format(script_name),
+    "End Process")
