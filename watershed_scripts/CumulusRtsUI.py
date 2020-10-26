9794a485375843d66c3190e7b4b15b1c307c6334
Jeff Gregory <jeffrey.s.gregory@me.com>
2020-10-26 13:07:22 -0500

    Updated and tested

--- watershed_scripts/CumulusRtsUI.py
+++ watershed_scripts/CumulusRtsUI.py
@@ -773,7 +773,4 @@ def main():
         cui.setVisible(True)
 
 if __name__ == "__main__":
-    # DELETE THIS LIST.  ONLY FOR TESTING
-    sys.argv[1:] = ["2020-09-01T12:00:00", "2020-09-14T12:00:00", "D:/WS_CWMS/lrn-m3000-v31-pro/database/grid.dss", "C:/app/CWMS/CWMS-Production/CAVI", "C:/Users/h3ecxjsg/AppData/Roaming/cumulus.config"]
-    # DELETE THIS LIST.  ONLY FOR TESTING
     main()
