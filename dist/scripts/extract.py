5b5d35e844fe25d2143aad72147a33e2af0f9414
Jeff Gregory <jeffrey.s.gregory@me.com>
2022-12-06 14:29:21 -0600

    Refactor extract timeseries CAVI script

--- dist/scripts/extract.py
+++ dist/scripts/extract.py
@@ -1,61 +1,186 @@
-"""Example Script: Extract from Water API
 """
+Extract timeseries data from Water API and save to DSS file
+"""
+
 import os
-from java.util import TimeZone
+from functools import partial
+import json
+from string import Template
+
+import java.lang.Class
+import java.sql.DriverManager
+import java.sql.SQLException
+import javax.swing.JOptionPane
+
 from hec.heclib.util import HecTime
-from hec.script import MessageBox
 
-from rtsutils.cavi.jython.ui import extract
-from rtsutils.cavi.jython import status
+from rtsutils import go
+from rtsutils.cavi.jython import jutil, status
+from rtsutils import USGS_SQL_DB
 
-# headless run True takes predefined configurations and
-# False gives the user a UI to define the configurations
-HEADLESS = False
+# set the provider
+PROVIDER = "" # your district (MSC)
 
-# set some initial parameters
-params_ = {
-    "Host": "develop-water-api.corps.cloud",
-    "Scheme": "https",
-    "Timeout": 120  # default is 300
-}
+# set the group
+GROUP = "" # timeseries_group defined in Water API
 
+# DSS APART default is an empty string
+apart = ""
 
-# shred directory is the 'shared' directory in the CAVI's open watershed
-SHARED = status.get_shared_directory()
-CONFIG = os.path.join(SHARED, "{}-cfg.json".format(arg2))
+# set the dss file, A part, and B part
+DSSFILENAME = os.path.join(
+    status.get_database_directory(),
+    "data.dss"
+)
 
+USGS_TIMESERIES = "usgs-timeseries"
+CWMS_TIMESERIES = "cwms-timeseries"
 
-# Get the time window from the CAVI
+# SQLITE, JDBC, and Queries
+JDBC_URL = "jdbc:sqlite:%s"  % USGS_SQL_DB
+JDBC_DRIVER = "org.sqlite.JDBC"
+DSS_ATTR_QUERY = Template("""
+SELECT dss_parameter, dss_type, dss_unit
+FROM dss as d
+WHERE d.usgs_code_id = (SELECT id FROM parameter_code as pc WHERE pc.code = '${PCODE}');
+""")
+
+# set the scheme, host, and timeout
+SCHEME = "https"
+HOST = "water-api.corps.cloud"
+TIMEOUT = 600
+
+# get the time window
+# after = "" # "YYYY-MM-DDTHH:MM:SSZ"
+# before = "" # "YYYY-MM-DDTHH:MM:SSZ"
 tw = status.get_timewindow()
 if tw != None:
     st, et = tw
+    after_hec = HecTime(st)
+    before_hec = HecTime(et)
+    after = "%d-%02d-%02dT%02d:%02d:00Z" % (after_hec.year(), after_hec.month(), after_hec.day(), after_hec.hour(),after_hec.minute())
+    before = "%d-%02d-%02dT%02d:%02d:00Z" % (before_hec.year(), before_hec.month(), before_hec.day(), before_hec.hour(),before_hec.minute())
     print("Time window: {}".format(tw))
 else:
-    MessageBox.showError("No forecast open on Modeling tab to get a timewindow.")
-    raise Exception("No forecast open on Modeling tab to get a timewindow.")
+    msg = "No Forecast open or in \"Setup Tab\""
+    javax.swing.JOptionPane.showMessageDialog(
+        None,
+        msg,
+        "No Forecast",
+        javax.swing.JOptionPane.ERROR_MESSAGE,
+    )
+    raise Exception(msg)
 
-# set the start time and convert to UTC
-ws_tz = status.get_timezone()
 
-st = HecTime(st, HecTime.MINUTE_GRANULARITY)
-st.showTimeAsBeginningOfDay(True)
-HecTime.convertTimeZone(st, ws_tz, TimeZone.getTimeZone('UTC'))
+# Go Configurations
+ENDPOINT = Template("providers/${PROVIDER}/timeseries_groups/${GROUP}/values")
+CONFIG = {
+    "Scheme": SCHEME,
+    "Host": HOST,
+    "StdOut": "true",
+    "Subcommand": "extract",
+    "Endpoint": ENDPOINT.substitute(PROVIDER=PROVIDER, GROUP=GROUP),
+    "After": after,
+    "Before": before,
+    "Timeout": 600,
+}
 
-et = HecTime(et, HecTime.MINUTE_GRANULARITY)
-et.showTimeAsBeginningOfDay(True)
-HecTime.convertTimeZone(et, ws_tz, TimeZone.getTimeZone('UTC'))
 
-after = "{}-{:02d}-{:02d}T{:02d}:{:02d}:00Z".format(st.year(), st.month(), st.day(), st.hour(), st.minute())
-before = "{}-{:02d}-{:02d}T{:02d}:{:02d}:00Z".format(et.year(), et.month(), et.day(), et.hour(), et.minute())
+# get the data and save
+sub = go.get(out_err=False, is_shell=False)
 
-params_["After"] = after
-params_["Before"] = before
+sub.stdin.write(json.dumps(CONFIG))
+sub.stdin.close()
 
-#
-cui = extract.Extract()
-cui.extract_configuration(CONFIG)
-cui.go_configuration(params_)
-if HEADLESS:
-    cui.execute()
-else:
-    cui.invoke()
+stderr = sub.stderr.read()
+sub.stderr.close()
+if "error" in stderr:
+    javax.swing.JOptionPane.showMessageDialog(
+        None,
+        stderr.split("::")[-1],
+        "Error",
+        javax.swing.JOptionPane.ERROR_MESSAGE,
+    )
+    quit()
+
+
+
+byte_array = bytearray()
+stmt = None
+conn = None
+# prepare sqlite db if needed with USGS datatype
+try:
+    java.lang.Class.forName(JDBC_DRIVER).newInstance()
+except Exception as e:
+    raise
+
+for iter_byte in iter(partial(sub.stdout.read, 1), b""):
+    byte_array.append(iter_byte)
+    if iter_byte == "}":
+        bpart = ""
+        cpart = ""
+        unit = ""
+        datatype = ""
+
+        obj = json.loads(str(byte_array))
+        byte_array = bytearray()
+
+        dtype = obj["datatype"]
+        key_parts = obj["key"].split(".")
+        times, values = list(zip(*obj["values"]))
+        try:
+            if conn is None or conn.isClosed():
+                conn = java.sql.DriverManager.getConnection(JDBC_URL)
+        except java.sql.SQLException as e:
+            raise
+
+        try:
+            stmt = conn.createStatement()
+            if dtype == USGS_TIMESERIES:
+                bpart, pcode = key_parts
+                result_set = stmt.executeQuery(DSS_ATTR_QUERY.substitute(PCODE=pcode))
+                while result_set.next():
+                    cpart = result_set.getString("dss_parameter")
+                    datatype = result_set.getString("dss_type")
+                    unit = result_set.getString("dss_unit")
+                    break
+
+            elif dtype == CWMS_TIMESERIES:
+                loc, para, ptype, interval, dur, version = key_parts
+            else:
+                raise Exception("Datatype {} not supported".format())
+        except Exception as e:
+            print(e)
+
+        hec_times = [
+            HecTime(t, HecTime.MINUTE_GRANULARITY).value()
+            for t in times
+        ]
+
+        msg = jutil.put_timeseries(
+            dssfilename=DSSFILENAME,
+            apart=apart,
+            bpart=bpart,
+            cpart=cpart,
+            fpart=USGS_TIMESERIES,
+            unit=unit,
+            datatype=datatype,
+            times=hec_times,
+            values=list(values)
+        )
+
+        if msg:
+            print(msg)
+
+
+if stmt:
+    stmt.close()
+if conn:
+    conn.close()
+
+javax.swing.JOptionPane.showMessageDialog(
+    None,
+    "Program Done",
+    "Program Done",
+    javax.swing.JOptionPane.INFORMATION_MESSAGE,
+)
