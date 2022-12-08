76cf5d2386e3ebf7729cc04a0c013859d207ca3c
Jeff Gregory <37386268+jeffsuperglide@users.noreply.github.com>
2022-12-08 14:32:43 -0600

    Refactor/extract script only (#112)
    * WIP: timeout default updated and added sqlite db
    
    * Refactor extract timeseries CAVI script
    
    * Move stderr read below stdout read; tested in CAVI

--- rtsutils/__init__.py
+++ rtsutils/__init__.py
@@ -6,6 +6,8 @@ import os.path
 # handle the jython poop
 null = None
 
+USGS_SQL_DB = os.path.abspath(os.path.join(os.path.dirname(__file__), "usgs/get_usgs.db"))
+
 APPDATA = os.path.expandvars("$APPDATA")
 
 PACKAGE_PATH = os.path.dirname(__file__)
