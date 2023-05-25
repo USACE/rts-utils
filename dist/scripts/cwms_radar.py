7620ac4b6c41a51a74cec9cf14a1f7ca88d83a45
Jeff Gregory <37386268+jeffsuperglide@users.noreply.github.com>
2023-05-25 07:04:11 -0600

    Refactor cwms_radar dist script (#117)

--- dist/scripts/cwms_radar.py
+++ dist/scripts/cwms_radar.py
@@ -3,6 +3,8 @@
 
 import os
 
+import threading
+
 from rtsutils.cavi.jython import status
 from rtsutils.cwmsradar import CwmsRADAR
 
@@ -12,21 +14,39 @@ from hec.script import MessageBox
 #
 tw = status.get_timewindow()
 if tw != None:
-    st, et = tw
-    print("Time window: {}".format(tw))
+	st, et = tw
+	print("Time window: {}".format(tw))
 else:
-    MessageBox.showError("No Forecast open or in 'Setup Tab'")
-    raise Exception("No Forecast open or in 'Setup Tab'")
+	MessageBox.showError("No Forecast open or in 'Setup Tab'")
+	raise Exception("No Forecast open or in 'Setup Tab'")
 
-cwmsdat = CwmsRADAR()
-cwmsdat.begintime = cwmsdat.format_datetime(HecTime(st))
-cwmsdat.endtime = cwmsdat.format_datetime(HecTime(et))
-cwmsdat.dssfile = os.path.join(
-    status.get_database_directory(),
-    "{}.dss".format(status.get_watershed().getName()),
-)
-cwmsdat.read_config(os.path.join(status.get_shared_directory(), "template_cwms_radar.config"))
-# Reading the configutation file defines the lists but they still need to be set
-cwmsdat.set_tsids()
-cwmsdat.run()
+class RadarThread(threading.Thread):
+	def __init__(self, st, et, tsid, dssid):
+		threading.Thread.__init__(self)
+		self.st = st
+		self.et = et
+		self.tsid = tsid
+		self.dssid = dssid
+
+	def run(self):
+		radar(self.st, self.et, self.tsid, self.dssid)
+
+def radar(st, et, tsid, dssid):
+		cwmsdat = CwmsRADAR()
+		cwmsdat.begintime = cwmsdat.format_datetime(HecTime(st))
+		cwmsdat.endtime = cwmsdat.format_datetime(HecTime(et))
+		cwmsdat.dssfile = os.path.join(
+			status.get_database_directory(),
+			"data.dss"
+		)
+		cwmsdat.set_tsids([tsid.strip()], [dssid.strip()])
+		cwmsdat.run()
+
+with open(os.path.join(status.get_shared_directory(), "cwms_radar.config")) as fp:
+	for line in fp.readlines():
+		tsid, dssid = line.split(":")
+		t = RadarThread(st, et, tsid, dssid)
+		t.start()
+		t.join()
+
 MessageBox.showInformation("Script Done", "Script Done")
